f4c05f8b67c1507c279caec925cad5ff7069f223
Samuel Olwe <samuel.olwe@canonical.com>
2024-02-14 18:53:29 +0300

    Get pr info after job info is loaded

--- webapp/jenkins.py
+++ webapp/jenkins.py
@@ -10,13 +10,22 @@ class Jenkins:
         self.jenkins_token = app.config.get("JENKINS_TOKEN")
         self.job_path = app.config.get("BUILD_URL").split(".com")[1]
         self.jenkins_job_url = f"http://{self.jenkins_url}{self.job_path}"
-        self.gh_url = self.get_pr_info()
         self.info = self.__get_job_info__()
 
     def __get_job_info__(self):
         url = f"{self.jenkins_job_url}api/python"
         response = requests.get(url)
-        return ast.literal_eval(response.text)
+        info = ast.literal_eval(response.text)
+        self.gh_url = self.__get_pr_info__(info)
+        return info
+
+    def __get_pr_info__(self, info):
+        for action in info["actions"]:
+            if "parameters" in action:
+                for param in action["parameters"]:
+                    if "PR_URL" in param["name"]:
+                        return param["value"]
+        return "No PR URL found"
 
     def get_job_logs(self):
         url = f"{self.jenkins_job_url}consoleText"
@@ -27,14 +36,6 @@ class Jenkins:
         domain, pr_no = self.gh_url.split("canonical/")[1].split("/pull/")
         domain = domain.replace(".", "-")
         return f"{domain}-{pr_no}.demos.haus"
-
-    def get_pr_info(self):
-        for action in self.info["actions"]:
-            if "parameters" in action:
-                for param in action["parameters"]:
-                    if "PR_URL" in param["name"]:
-                        return param["value"]
-        return "No PR URL found"
 
     def get_start_time(self):
         return datetime.fromtimestamp(self.info["timestamp"] / 1000).strftime(
