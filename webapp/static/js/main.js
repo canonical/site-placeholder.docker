b3e7c52cc06ba16cd47321db4c30653f0c93634f
Samuel Olwe <samuel.olwe@canonical.com>
2024-02-14 12:42:45 +0300

    Enabled restart button

--- webapp/static/js/main.js
+++ webapp/static/js/main.js
@@ -20,3 +20,14 @@ function toggleRestartButton() {
     }   
 }
 window.addEventListener('load', toggleRestartButton);
+
+function submitRestartRequest() {
+    let button = document.getElementById('restart-button');
+    button.setAttribute("disabled", "");
+    button.setAttribute("data-building", "True");
+    button.innerHTML = "Restarting...";
+
+    let xhr = new XMLHttpRequest();
+    xhr.open("GET", "/restart-build", (result) => result.responseText);
+    xhr.send();
+}
