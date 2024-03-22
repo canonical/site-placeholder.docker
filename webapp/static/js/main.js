46714a7e7202bfe358d0ff8a35f8b15f95269553
Samuel Olwe <sam.olwe@gmail.com>
2024-03-22 15:06:32 +0300

    Removed auto reload

--- webapp/static/js/main.js
+++ webapp/static/js/main.js
@@ -1,33 +1,27 @@
-// Refresh the screen every 10 seconds
-function refresh() {
-    location.reload();
-}
-setTimeout(refresh, 10000);
-
 // Scroll to the bottom of the logs
 function scrollLogs() {
-    let codeBox = document.getElementById('code-box');
-    codeBox.scrollTo(0, codeBox.offsetTop);
+  let codeBox = document.getElementById("code-box");
+  codeBox.scrollTo(0, codeBox.offsetTop);
 }
-window.addEventListener('load', scrollLogs);
+window.addEventListener("load", scrollLogs);
 
 // Disable restart button if build is currently in progress
 function toggleRestartButton() {
-    let button = document.getElementById('restart-button');
+  let button = document.getElementById("restart-button");
 
-    if (button.getAttribute("data-building") === "True") {
-        button.setAttribute("disabled", "");
-    }   
+  if (button.getAttribute("data-building") === "True") {
+    button.setAttribute("disabled", "");
+  }
 }
-window.addEventListener('load', toggleRestartButton);
+window.addEventListener("load", toggleRestartButton);
 
 function submitRestartRequest() {
-    let button = document.getElementById('restart-button');
-    button.setAttribute("disabled", "");
-    button.setAttribute("data-building", "True");
-    button.innerHTML = "Restarting...";
+  let button = document.getElementById("restart-button");
+  button.setAttribute("disabled", "");
+  button.setAttribute("data-building", "True");
+  button.innerHTML = "Restarting...";
 
-    let xhr = new XMLHttpRequest();
-    xhr.open("GET", "/restart-build", (result) => result.responseText);
-    xhr.send();
+  let xhr = new XMLHttpRequest();
+  xhr.open("GET", "/restart-build", (result) => result.responseText);
+  xhr.send();
 }
